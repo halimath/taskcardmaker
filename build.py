bbe8793f175c397133989330a49fe3d6a9b9b533
halimath <halimath.wilanthaou@googlemail.com>
2012-06-15 14:27:13 +0200

    incremented version

--- build.py
+++ build.py
@@ -16,7 +16,7 @@ use_plugin("python.django")
 
 default_task = ["analyze", "publish"]
 
-version = "0.4.4"
+version = "0.4.6"
 summary = "A task card generator"
 authors = [Author("Alexander Metzner", "alexander.metzner@gmail.com")]
 
